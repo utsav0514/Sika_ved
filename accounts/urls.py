6a67dccb1bfd850bd83a887da9e74ff449af6ef2
utsav <utshavshrestha.1713@gmail.com>
2025-10-31 14:20:22 +0545

    Expense managment system

---
+++ accounts/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from .views import register
+
+urlpatterns = [
+    path('register/', register, name='register'),
+
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+
+   
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+]
---
+++ accounts/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from .forms import UserCreateForm  
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreateForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  
+            return redirect('home') 
+    else:
+        form = UserCreateForm()
+
+    return render(request, 'accounts/register.html', {'form': form})
