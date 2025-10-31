1db272005aeca3f3691fd75b17d5b886189a430e
utsav <utshavshrestha.1713@gmail.com>
2025-10-31 15:14:48 +0545

    adding login page

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,7 +5,7 @@ from .views import register
 urlpatterns = [
     path('register/', register, name='register'),
 
-    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
 
    
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
