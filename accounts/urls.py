3fc5a47c7a8c62ebfbe60d303d2e5859bd6f6fb7
utsav <utshavshrestha.1713@gmail.com>
2025-11-01 11:10:07 +0545

    update in logout logic

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,10 +3,12 @@ from django.contrib.auth import views as auth_views
 from .views import register
 
 urlpatterns = [
+
     path('register/', register, name='register'),
+
 
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
 
-   
+
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
